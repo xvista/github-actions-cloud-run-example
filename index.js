69939f7b85411bd533043e942a494bb44d3954e5
Atikhun Orisoon <genatorster@gmail.com>
2020-05-10 01:00:36 +0700

    Try to change response

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+  res.send(`Hello ${target}! This is my first experience on Cloud Run!`);
 });
 
 const port = process.env.PORT || 8080;
